382728a5da70a89e5288a25f8269c8ac36f954cb
Iria Carro <iria.com@hotmail.com>
2019-09-16 13:16:55 +0200

    Fix on crateddate variable added

--- app/crawlers/highsnobiety.js
+++ app/crawlers/highsnobiety.js
@@ -22,7 +22,7 @@ function getArticles($) {
         const title = item('.teaser__title').text().replace('\t', '').replace('\n', '').trim();
         const date = item('.teaser__meta--date').text();
         const url = item('.teaser__title')[0].attribs.href;
-        return { title: title, date: date, url: url, type: 0, sent: false, createddate = new Date()};
+        return { title: title, date: date, url: url, type: 0, sent: false, createddate: new Date()};
     });
 }
 
