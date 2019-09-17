673fc375445403da6c1f4232bdc6b7db1fd30193
Iria Carro <iria.com@hotmail.com>
2019-09-17 11:15:46 +0200

    Added genbeta crawler

--- app/index.js
+++ app/index.js
@@ -10,6 +10,7 @@ const vidaextragetarticles = require('./crawlers/vidaextra');
 const vicegetarticles = require('./crawlers/vice');
 const hypebeastgetarticles = require('./crawlers/hypebeast');
 const xatakagetarticles = require('./crawlers/xataka');
+const genbetagetarticles = require('./crawlers/genbeta');
 const SendNews = require('./helper/sender');
 const processArticles = require("./helper/articles");
 
@@ -47,7 +48,7 @@ async function initScheduler() {
             cron.scheduleJob(ruleCrawler, function(){
                 console.log('Crawler working');
                 crawler.queue({ uri: 'https://themedizine.com/categoria/musica/?utm_source=web&utm_medium=slider&utm_campaign=category', type: 'themedizine' });
-                crawler.queue({ uri: 'https://www.applesfera.com/', type: 'applesfera' });
+                crawler.queue({ uri: 'https://www.applesfera.com/', type: 'xataka' });
                 crawler.queue({ uri: 'https://www.highsnobiety.com/', type: 'highsnobiety'});
                 crawler.queue({ uri: 'https://www.25gramos.com/category/news/', type: '25gramos'});
                 crawler.queue({ uri: 'https://www.25gramos.com/category/lectura/', type: '25gramos'});
@@ -55,6 +56,7 @@ async function initScheduler() {
                 crawler.queue({ uri: 'https://www.vice.com/es', type: 'vice'});
                 crawler.queue({ uri: 'https://hypebeast.com/', type: 'hypebeast'});
                 crawler.queue({ uri: 'https://www.xataka.com/', type: 'xataka'});
+                crawler.queue({ uri: 'https://www.genbeta.com/', type: 'xataka'});
             });
             cron.scheduleJob(ruleSender, function(){
                 console.log('Sender working');
